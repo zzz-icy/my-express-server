6c5d2a73d96700b5bb4ab3efdd3d68fb48f3176a
Hannah Z <eveline1129@163.com>
2024-08-23 11:20:10 -0500

    clean up for secrete project

--- solution.js
+++ solution.js
@@ -2,29 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-var bandName = "";
+
+var userIsAuthorised = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function bandNameGenerator(req, res, next) {
-  console.log(req.body);
-  bandName = req.body["street"] + req.body["pet"];
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
   next();
 }
-
-app.use(bandNameGenerator);
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
