494c991b360018287bac779e9e731ab13082cf40
Hannah Z <eveline1129@163.com>
2024-08-26 23:39:38 -0500

    added PATCH DELETE

--- joke_api/index.js
+++ joke_api/index.js
@@ -51,14 +51,49 @@ app.put("/jokes/:id", (req, res) => {
 		jokeText: req.body.jokeText,
 	}
 	jokes[searchIndex] = replacementJoke
-	res.json(jokes)
+	res.json(replacementJoke)
 })
 
 //6. PATCH a joke
+app.patch("/jokes/:id", () => {
+	const id = parseInt(request.params.id)
+	const searchIndex = jokes.findIndex((joke) => joke.id === id)
+	const originalJoke = jokes[searchIndex]
+	const replacementJoke = {
+		id,
+		jokeText: req.body.jokeText || originalJoke.jokeText,
+		jokeType: req.body.jokeType || originalJoke.jokeType,
+	}
+	jokes[searchIndex] = replacementJoke
+	console.log(jokes[searchIndex])
+	res.json(replacementJoke)
+})
 
 //7. DELETE Specific joke
-
+app.delete("/jokes/:id", (req, res) => {
+	const id = parseInt(request.params.id)
+	const searchIndex = jokes.findIndex((joke) => joke.id === id)
+	if (searchIndex > -1) {
+		jokes.splice(searchIndex, 1)
+		res.sendStatus(200)
+	} else {
+		res
+			.status(404)
+			.json({ error: `Joke with id: ${id} not found. No jokes were deleted.` })
+	}
+})
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+	const userKey = req.query.key
+	if (userKey === masterKey) {
+		jokes = []
+		res.sendStatus(200)
+	} else {
+		res
+			.status(404)
+			.json({ error: `You are not authorised to perform this action.` })
+	}
+})
 
 app.listen(port, () => {
 	console.log(`Successfully started server on port ${port}.`)
